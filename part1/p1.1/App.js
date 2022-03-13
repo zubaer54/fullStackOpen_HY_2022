62f43138a27bfcf382dcba03d2fba0bfb0fa1903
Zubaer Halim <zubaer.e.h@gmail.com>
2022-03-13 04:04:18 -0300

    Create App.js

---
+++ part1/p1.1/App.js
@@ -0,0 +1,38 @@
+const Header = (cN) => (
+  <>
+  <h1>{cN.nam}</h1>
+  </>
+)
+
+const Content = (pN) => (
+  <>
+  <p>{pN.nam} {pN.exer}</p>
+  </>
+)
+
+const Total = (tot) => (
+  <>
+  <p>Number of exercises {tot.ex1 + tot.ex2 + tot.ex3}</p>
+  </>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header nam = {course}/>
+      <Content nam = {part1} exer = {exercises1}/>
+      <Content nam = {part2} exer = {exercises2}/>
+      <Content nam = {part3} exer = {exercises3}/>
+      <Total ex1 = {exercises1} ex2 = {exercises2} ex3 = {exercises3}/>
+    </div>
+  )
+}
+export default App
