2849e84a21ac98208723bdaae54b2d73ed3943f1
Zubaer Halim <zubaer.e.h@gmail.com>
2022-03-21 02:21:52 -0300

    Create App.js

---
+++ part2/p7/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+    const newPersonObj = {
+      name: newName
+    }
+    if (persons.some (person => person.name === newName)) {
+      window.alert(`${newName} is already added to phonebook`);
+    }
+    else {
+      setPersons(persons.concat(newPersonObj))
+      setNewName('')
+    }
+  }
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit = {addPerson}>
+        <div>
+          name: <input 
+            value = {newName}
+            onChange = {handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <div key = {person.name}>{person.name}</div>)}
+    </div>
+  )
+}
+export default App
