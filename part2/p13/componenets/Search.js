fdc3a5cbcffdb781d390626ad8946d2e39cc5b3d
Zubaer Halim <zubaer.e.h@gmail.com>
2022-03-29 21:04:05 -0300

    Update Search.js

--- part2/p13/componenets/Search.js
+++ part2/p13/componenets/Search.js
@@ -1,20 +1,17 @@
 const Search = (props) => {
-    const handleChange = (e) => {
-    console.log(e.target.value)
-    const keyword = e.target.value
+  const handleChange = (e) => {
+    const keyWord = e.target.value
     const filterItems = (arr, que) => {
-        return arr.filter(el => el.toLowerCase().indexOf(que.toLowerCase()) !== -1)
+      return arr.filter(el => el.toLowerCase().indexOf(que.toLowerCase()) !== -1)
     }
-    props.setFoundCountry(filterItems(props.listOfCountries, keyword))
-    }
-    return (
-      <form>
-        <div>
-          find countries: <input
-              onChange = {handleChange}/>
-        </div>
-      </form>
-    )
+    props.setFoundCountry(filterItems(props.listOfCountries, keyWord))
+  }
+  return (
+    <form>
+      <div>
+        find countries: <input onChange = {handleChange}/>
+      </div>
+    </form>
+  )
 }
-
 export default Search
