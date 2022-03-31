01e213acabac60e360939e7213fee0f83b548ea8
Zubaer Halim <zubaer.e.h@gmail.com>
2022-03-30 22:45:34 -0300

    Create Weather.js

---
+++ part2/p14/components/Weather.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const Weather = (props) => {
+    const [w, setW] = useState([])
+    const [isLoading, setLoading] = useState(true)
+    const city = props.cap[0]
+    const api_key = '5fd1c330f2d7a8b5c51e5b3aa0ee7677'
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+
+    useEffect(() => {
+        axios
+        .get(url)
+        .then(response => {
+          setW(response.data)
+          setLoading(false)
+          console.log(response.data)
+        })
+    }, [])
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div>
+            <h3>Weather in {city}</h3>
+            <p>temperature {w.main.temp} Celsius <br></br>
+            <img src = {`http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png`} /><br></br>
+            wind {w.wind.speed} m/s</p>
+        </div>
+    )
+}
+export default Weather
