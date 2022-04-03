5934cf52a4f9734db1e67c7030c7ab20a223a5c0
Zubaer Halim <zubaer.e.h@gmail.com>
2022-04-03 02:54:51 -0300

    Update App.js

--- part2/practise/d/App.js
+++ part2/practise/d/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -10,11 +11,11 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
         console.log('promise fulfilled')
-        setNotes(response.data)
+        setNotes(initialNotes)
         setLoading(false)
       })
   }, [])
@@ -24,23 +25,22 @@ const App = () => {
 
   const handleChange = (e) => {
     console.log(e.target.value)
-    setNewNote(e.target.value)
-
-    return 
+    return (
+      setNewNote(e.target.value)
+    )
   }
   const addNote = (e) => {
     e.preventDefault()
     const noteObj = {
       content: newNote,
-      date: new Date(),
+      date: new Date().toISOString(),
       important: Math.random() < 0.5
     }
-    axios
-      .post('http://localhost:3001/notes', noteObj)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObj)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
-        console.log(response)
       })
   }
   
@@ -49,10 +49,15 @@ const App = () => {
     const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-    axios
-      .put(url, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => note.id !== id ? note : response.data))
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note ${note.content} is hardcoded`
+        )
       })
   }
   
